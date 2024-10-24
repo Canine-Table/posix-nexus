6ef98997687f337b2655cef12242371aec568c19
Local User <user@home.lab>
2024-10-24 17:37:33 -0400

    testing daemon

--- main/sh/lib/posix-nexus.sh
+++ main/sh/lib/posix-nexus.sh
@@ -1,57 +1,53 @@
 posixNexusDaemon() {
-
-    _replace() {
-
-        # Replace occurrences of match_value with replace_value using AWK
-        echo -n "${1}" | awk \
-            -v match_value="${2}" \
-            -v replace_value="${3}" \
-        '{
-            gsub(match_value, replace_value);
-            print;
+    
+    eval "$(
+        cat "${POSIX_NEXUS_ROOT}/${1}" | ${AWK} '{
+            if ($0 !~ /^[[:space:]]*startPosixNexus[[:space:]]*\(\)[[:space:]]*\{/) {
+                print $0;
+            } else {
+                exit 0;
+            }
         }'
+    )"
 
-    }
- _replace "hello world" "world" "hello"
- exit
-    # echo "line 34 posix-nexus.sh";
-    # exit;
-#    _posixNexusDaemonGlobals;
+   _import $(_posixNexusLinker sh);
 
-    {
+    # Set file creation mask
+    umask 027;
 
-        # Set file creation mask
-        umask 027;
+    while ! { ps -A -o pid | ${AWK} '{ gsub(/(^[[:space:]]+)|([[:space:]]+$)/, ""); print; }' | grep -q "^$(cat "${POSIX_NEXUS_PID}")$"; }; do
+        :;
+    done
 
-        export POSIX_NEXUS_PID="/var/run/posix-nexus/posix-nexus.pid";
-        export POSIX_NEXUS_LINK="/var/run/posix-nexus/posix-nexus-directory";
+    _import $(_posixNexusLinker sh);
 
-        [ -h "${POSIX_NEXUS_LINK}" ] && {
-           try -O "
-                C = unlink, 
-                U = ${POSIX_NEXUS_LINK}
-            " || exit;
-        }
+    export POSIX_NEXUS_DAEMON_ROOT="/var/tmp/posix-nexus/$(cat "${POSIX_NEXUS_PID}")";
 
-        nohup ${SHELL} -c "
-            _import $(_posixNexusLinker sh);
+    try -O "
+        D = ${POSIX_NEXUS_DAEMON_ROOT}
+    ";
 
-            while [ -z '${POSIX_NEXUS_DAEMON_ROOT}' ]; do
-                sleep 2;
-                echo hi;
-            done
+    (
+        # Clean up old directories and start the daemon
+        for OLD in /var/tmp/posix-nexus/*; do
+            [ "${OLD}" = "${POSIX_NEXUS_DAEMON_ROOT}" ] || rm -rf "${OLD}";
+        done
+    ) &
 
-        " & printf "%d" $! > "${POSIX_NEXUS_PID}";
+    export POSIX_NEXUS_STDIN="${POSIX_NEXUS_DAEMON_ROOT}/stdin-posix-nexus.in";
+    export POSIX_NEXUS_STDOUT="${POSIX_NEXUS_DAEMON_ROOT}/stdout-posix-nexus.out";
+    export POSIX_NEXUS_STDERR="${POSIX_NEXUS_DAEMON_ROOT}/stderr-posix-nexus.out";
 
-        # Set environment variables for various paths
-        export POSIX_NEXUS_DAEMON_ROOT="/var/tmp/posix-nexus/$(cat "${POSIX_NEXUS_PID}")";
-        export POSIX_NEXUS_STDIN="${POSIX_NEXUS_DAEMON_ROOT}/stdin-posix-nexus.in";
-        export POSIX_NEXUS_STDOUT="${POSIX_NEXUS_DAEMON_ROOT}/stdout-posix-nexus.out";
-        export POSIX_NEXUS_STDERR="${POSIX_NEXUS_DAEMON_ROOT}/stderr-posix-nexus.out";
+    try -O "
+        C = mkfifo,
+        F = ${POSIX_NEXUS_STDIN},
+        S = ${POSIX_NEXUS_DAEMON_ROOT} : ${POSIX_NEXUS_LINK}
+    ";
 
-#        try -O "S = ${POSIX_NEXUS_DAEMON_ROOT} : ${POSIX_NEXUS_LINK}";
-        try -C 'U=run.sh';
-
-    } || exit;
+    while :; do
+        cat "${POSIX_NEXUS_STDIN}" &
+    done 1>"${POSIX_NEXUS_STDOUT}" 2>"${POSIX_NEXUS_STDERR}";
 
 }
+
+posixNexusDaemon "${1}" ""
