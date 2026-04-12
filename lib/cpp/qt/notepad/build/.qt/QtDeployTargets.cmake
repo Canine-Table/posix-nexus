f57910eebe71a77a5a82ff99a282ec00e09c8bd2
Canine Table <caninetable@outlook.com>
2026-04-11 22:08:46 -0400

    update gitignore

--- lib/cpp/qt/notepad/build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_notepad_FILE /opt/posix-nexus/lib/cpp/qt/notepad/build/notepad)
-set(__QT_DEPLOY_TARGET_notepad_TYPE EXECUTABLE)
--- lib/cpp/qt/notepad/build/CMakeFiles/notepad.dir/notepad_autogen/mocs_compilation.cpp.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/notepad.dir/notepad_autogen/mocs_compilation.cpp.o: \
- /opt/posix-nexus/lib/cpp/qt/notepad/build/notepad_autogen/mocs_compilation.cpp \
- /usr/include/stdc-predef.h
